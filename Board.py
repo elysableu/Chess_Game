334ad5d6e5b5d6e5427855ebb907aadab0572246
theandyman007 <andrew.richardson4422@gmail.com>
2023-08-28 16:03:15 -0600

    added to chess board python class

--- Board.py
+++ Board.py
@@ -1,13 +1,17 @@
 
+from typing import Self
+
+
 class my_class(object):
     
-    board = [["empty" for letter in range(8)] for number in range(8)]
+    _board = [["empty" for letter in range(8)] for number in range(8)]
 
 
 
 
 
     def __init__(self, NewBoard = True):
+        #set up a new board with all pieces in their starting location
         if NewBoard:
             """
             pawn:   (1,0-7) (6, 0-7)
@@ -20,16 +24,32 @@ class my_class(object):
 
             for letter in range(8):
                 for number in range(8):
-                    self.board[letter][number] = "empty"
+                    self._board[letter][number] = "empty"
 
             for number in range(8):
-                self.board[1][number] = "pawn"
-                self.board[6][number] = "pawn"
+                self._board[1][number] = "pawn"
+                self._board[6][number] = "pawn"
+
+            self._board[0][0] = self._board[0][7] = self._board[7][0] = self._board[7][7] = "rook"
+            self._board[0][1] = self._board[0][6] = self._board[7][1] = self._board[7][6] = "bishop"
+            self._board[0][2] = self._board[0][5] = self._board[7][2] = self._board[7][5] = "knight"
+            self._board[0][3] = self._board[7][3] = "queen"
+            self._board[0][4] = self._board[7][4] = "king"
+
+        #what to do if this is not for a new game
+        #possibly implement a way to set up board based on a previous boardstate
+        #NOT CURRENTLY IMPLEMENTED
+        if not NewBoard:
+            pass
 
 
 
+    def getBoardState():
+        pass
 
-
+    def movePiece():
+        #code to move a piece
+        pass
 
 
 
